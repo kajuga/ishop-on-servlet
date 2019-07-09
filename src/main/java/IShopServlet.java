1bfdaa94e98230e205c8b896e99322a0e2d7af32
kajuga <kajuga@mail.ru>
2019-07-09 15:51:58 +0300

    first init

---
+++ src/main/java/IShopServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class IShopServlet extends HttpServlet {
+    private static final long serialVersionUID = -31338536290634165L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("Hello World!");
+        out.println("</body></html>");
+        out.close();
+    }
+}
