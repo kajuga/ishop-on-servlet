8aa461bac90b68643fc5ffa46351d3bc780ce3e0
kajuga <kajuga@mail.ru>
2019-07-11 16:03:19 +0300

    Homework Создать AdminServlet (‘/admin’), который должен проверить следующие параметры: 1)Если ip адрес клиента равен IP, то вывести в консоль Login via ip: %s и отобразить клиенту ОК; 2)Если header[ACCESS_KEY] равен ACCESSKEY, то вывести в консоль Login via accessKey: %s и отобразить клиенту ОК; 3)Если param[login]=admin, param[password]=password, то вывести в консоль Login via login and password: %s/%s и отобразить клиенту ОК; 4)Иначе вывести клиенту FAILED, 401 статус и в консоль отобразить текущее состояние всех данных проверки; 5)Эталонные параметры задать через Init параметры! 6)Проверить работу сервлета

--- src/main/java/sashokTestServlet/Servlet3.java
+++ src/main/java/sashokTestServlet/Servlet3.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class Servlet3 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.getSession().setAttribute("attr1", "serv3");
         resp.sendRedirect("/ishop-on-servlet/hello-world");
     }
 }
