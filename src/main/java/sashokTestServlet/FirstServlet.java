eb75844ef88c311ec026591020c61b2c22bcf2f5
kajuga <kajuga@mail.ru>
2019-07-25 11:21:45 +0300

    example of html coloring and other stuff

--- src/main/java/sashokTestServlet/FirstServlet.java
+++ src/main/java/sashokTestServlet/FirstServlet.java
@@ -21,12 +21,9 @@ public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter pw = response.getWriter();
         pw.println("<html>");
-        pw.println("<h1> Hello world! </h1>");
-        pw.println("<h3>ADDDDD </h3>");
-
-
-
-
+        pw.println("<h1 style=\"background-color:DodgerBlue;\"> Hello world! </h1>");
+        pw.println("<h3 style=\"color:Tomato;\">SERVLET FROM Alishev cources! </h3>");
+        pw.println("<h2 style=\"border:2px solid DodgerBlue;\">TEST BORDER!</h3>");
         pw.println("</html>");
 
     }
