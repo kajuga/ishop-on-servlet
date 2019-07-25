9c0bd6f98ee1a5ddf1a8f26a022d140c5d5602a9
kajuga <kajuga@mail.ru>
2019-07-25 13:12:28 +0300

    redirect && forvard simple stuff with jsp and not

--- src/main/java/sashokTestServlet/FirstServlet.java
+++ src/main/java/sashokTestServlet/FirstServlet.java
@@ -1,5 +1,6 @@
 package sashokTestServlet;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,12 +20,27 @@ public class FirstServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
         PrintWriter pw = response.getWriter();
         pw.println("<html>");
-        pw.println("<h1 style=\"background-color:DodgerBlue;\"> Hello world! </h1>");
+        pw.println("<h1 style=\"background-color:DodgerBlue;\"> Hello, " + name + " " + surname +" </h1>");
         pw.println("<h3 style=\"color:Tomato;\">SERVLET FROM Alishev cources! </h3>");
         pw.println("<h2 style=\"border:2px solid DodgerBlue;\">TEST BORDER!</h3>");
         pw.println("</html>");
 
+//        response.sendRedirect("https://www.google.com");
+//        response.sendRedirect("/ishop-on-servlet/testJsp.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/testJsp.jsp");
+        dispatcher.forward(request,response);
+
     }
 }
+
+
+
+
+
+
+
+
