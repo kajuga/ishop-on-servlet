bf2f1c544e45234786ed4de961c893755d59ece9
kajuga <kajuga@mail.ru>
2019-08-15 20:13:19 +0300

    15/08/19 Listener homework

--- src/main/java/sashok/Constants.java
+++ src/main/java/sashok/Constants.java
@@ -1,11 +1,15 @@
 package sashok;
 
+import java.io.StringWriter;
+
 public final class Constants {
     public static final String CURRENT_SHOPPING_CART = "CURRENT_SHOPPING_CART";
 
     public static final int MAX_PRODUCT_COUNT_PER_SHOPPING_CART = 10;
 
     public static final int MAX_PRODUCTS_PER_SHOPPING_CART = 20;
+
+    public  static final String ACCOUNTS_ACTIONS_HISTORY = "ACCOUNTS_ACTIONS_HISTORY";
 
     public enum Cookie {
         //1 year ttl
